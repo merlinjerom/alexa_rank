8b213923a634b8c2469b752686f441906be57916
merlinjerom <merlin.jerom@gmail.com>
2016-12-27 07:39:38 +0530

    first commit

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -4,6 +4,7 @@ class Domain < ApplicationRecord
   validates :name, presence: true
   validate :allow_three_for_user
 
+
   def allow_three_for_user
     if Domain.where(user_id: self.user_id).count > 2
       errors.add(:name, "allow only three")
